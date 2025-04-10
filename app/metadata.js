47e149f678a225aca5cedd05ba0be795ce1ec32b
sofianeghn <165678280+sofianeghn@users.noreply.github.com>
2025-04-10 18:29:50 -0400

    Add - css modules/ main metadata.js

---
+++ app/metadata.js
@@ -0,0 +1,5 @@
+export const metadata = {
+    title: 'Rock en Bois Ottawa - Festival de Rock Acoustique',
+    description: 'Le meilleur de la musique acoustique dans un cadre intimiste à Ottawa',
+    keywords: 'rock acoustique, festival, Ottawa, musique live, concerts',
+}
